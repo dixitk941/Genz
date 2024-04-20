da07f3010347da572b2d79bb0db6e5c3a20fc42d
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-21 02:15:58 +0530

    Update laby.js

--- public/Room/laby.js
+++ public/Room/laby.js
@@ -1,20 +1,14 @@
 var firebaseConfig = {
     apiKey: "AIzaSyCYdeigPuvdKyVj0rG53IoUZHaVyawyVZY",
-
     authDomain: "genzconnect-f5082.firebaseapp.com",
-
     databaseURL: "https://genzconnect-f5082-default-rtdb.firebaseio.com",
-
     projectId: "genzconnect-f5082",
-
     storageBucket: "genzconnect-f5082.appspot.com",
-
     messagingSenderId: "761396074376",
-
     appId: "1:761396074376:web:ce2c9a3560a78194698c3d",
-
     measurementId: "G-ZFNR03PDPM",
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -38,28 +32,8 @@ async function launchlab(roomId) {
         }
     );
     document.getElementById("lab").setAttribute("src", content.one);
+    initWebRTC(roomId);
 }
-
-/*function getlab(roomId) {
-    const val = roomId;
-    console.log("getting the lab");
-    firebaseOrdersCollection.on("value", (data) => {
-        var scores = data.val();
-        console.log(scores);
-        var keys = Object.keys(scores);
-        for (var i = 0; i < keys.length; i++) {
-            var k = keys[i];
-            if (keys[i] == roomId) {
-                console.log("found");
-                var two = scores[k].two;
-                document.getElementById("lab").setAttribute("src", two);
-                break;
-            }
-        }
-    });
-}
-*/
-
 
 function getlab(roomId) {
     var val = roomId;
@@ -78,7 +52,68 @@ function getlab(roomId) {
                 var two = scores[k].two;
                 document.getElementById("lab").setAttribute("src", two);
                 console.log(two);
+                initWebRTC(roomId);
             }
         }
+    }
+}
+
+function initWebRTC(roomId) {
+    const configuration = {
+        iceServers: [{
+            urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+        }],
+        iceCandidatePoolSize: 10,
+    };
+
+    let peerConnection = null;
+    let localStream = null;
+    let remoteStream = null;
+    let roomRef = null;
+
+    makeRoom(roomId);
+
+    function makeRoom() {
+        localStream = new MediaStream();
+        document.getElementById("localVideo").srcObject = localStream;
+
+        const db = firebase.firestore();
+        roomRef = db.collection("rooms").doc(roomId);
+
+        peerConnection = new RTCPeerConnection(configuration);
+        peerConnection.addEventListener("icecandidate", handleIceCandidate);
+        peerConnection.addEventListener("track", handleTrack);
+
+        localStream.getTracks().forEach(track => {
+            peerConnection.addTrack(track, localStream);
+        });
+
+        peerConnection.addEventListener("iceconnectionstatechange", handleIceConnectionStateChange);
+        peerConnection.addEventListener("connectionstatechange", handleConnectionStateChange);
+        peerConnection.addEventListener("signalingstatechange", handleSignalingStateChange);
+    }
+
+    async function handleIceCandidate(event) {
+        if (event.candidate) {
+            const candidate = event.candidate.toJSON();
+            await roomRef.collection("candidates").add(candidate);
+        }
+    }
+
+    function handleTrack(event) {
+        const stream = event.streams[0];
+        document.getElementById("remoteVideo").srcObject = stream;
+    }
+
+    function handleIceConnectionStateChange() {
+        console.log(`ICE connection state changed to ${peerConnection.iceConnectionState}`);
+    }
+
+    function handleConnectionStateChange() {
+        console.log(`Connection state changed to ${peerConnection.connectionState}`);
+    }
+
+    function handleSignalingStateChange() {
+        console.log(`Signaling state changed to ${peerConnection.signalingState}`);
     }
 }
